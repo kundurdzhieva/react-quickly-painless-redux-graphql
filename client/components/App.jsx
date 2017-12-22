6cb3658980e0c83552ef45bd678491d1c226555c
Alexandar Kundurdzhiev <alexandar.kundurdzhiev@emerchantpay.com>
2017-12-22 15:58:57 +0200

    setup commit

---
+++ client/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Tooltip from 'Tooltip';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div style={{textAlign: 'center'}}>
+
+                <h1>Tooltip Widget</h1>
+                <Tooltip text="The book you are reading now">React Quickly</Tooltip>
+                was published in 2017. It's awesome!
+
+            </div>);
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './client/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/},
+            {test: /\.css$/, loader: 'style-loader!css-loader'},
+            {test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000'}
+        ]
+    },
+    plugins: [
+        HtmlWebpackPluginConfig,
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ],
+    resolve: {
+        modules: ["node_modules", "client/components"],
+        extensions: [".js", ".jsx", " "]
+    }
+};
+
