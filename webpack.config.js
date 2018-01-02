96c42c4f4c835e40f4b8600607d25331913dcf65
Alexandar Kundurdzhiev <alexandar.kundurdzhiev@emerchantpay.com>
2018-01-02 13:52:07 +0200

    added devtool inline map

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
             {test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000'}
         ]
     },
+    devtool: "inline-source-map",
     plugins: [
         HtmlWebpackPluginConfig,
         new webpack.ProvidePlugin({
